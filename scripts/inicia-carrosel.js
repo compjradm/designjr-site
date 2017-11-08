ea3a6f07df1d208857ca4ecf57823a09f343c015
joao vitor <94joaovitorls@gmail.com>
2017-11-08 09:20:36 -0200

    alteracoes responsividade

--- scripts/inicia-carrosel.js
+++ scripts/inicia-carrosel.js
@@ -6,10 +6,10 @@ $(document).ready(function(){
       navText: ["<span class='carousel-control-prev-icon' aria-hidden='true'></span>", "<span class='carousel-control-next-icon' aria-hidden='true'></span>"],
       responsive:{
          0:{
-            items:1
+            items:3
          },
          767:{
-            items:1
+            items:3
          },
          1023:{
             items:3
@@ -23,10 +23,10 @@ $(document).ready(function(){
       navText: ["<span class='carousel-control-prev-icon' aria-hidden='true'></span>", "<span class='carousel-control-next-icon' aria-hidden='true'></span>"],
       responsive:{
          0:{
-            items:1
+            items:3
          },
          767:{
-            items:1
+            items:3
          },
          1023:{
             items:3
--- scripts/troca-txt-carrosel-paginicial.js
+++ scripts/troca-txt-carrosel-paginicial.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 	});
 	//detecta o novo item para colocar o .gif
 	owl1.on('translated.owl.carousel', function(event) {
-		let altNova = $('.center > img.img-historia').attr("alt");
+		let altNova = $('.center > img.img-historia').attr("alt"); //seleciona imagem pelo atributo alt
 		$('.center > img.img-historia').attr('src', './imgs/historia/'+altNova+'-animado.gif');
 		if (altNova == "01") $('#troca-historia').replaceWith('<p id="troca-historia">Somos a empresa júnior de design da UNESP de Bauru.</p>');
 		if (altNova == "02") $('#troca-historia').replaceWith('<p id="troca-historia">Em 2000 foi apresentado um TCC sobre Gestão em Design. Graças a ele nossa empresa foi fundada no ano seguinte.</p>');
@@ -18,7 +18,6 @@ $(document).ready(function(){
 		if (altNova == "04") $('#troca-historia').replaceWith('<p id="troca-historia">A DJR é composta por estudantes de design que trabalham juntos de forma horizontal.</p>');
 		if (altNova == "05") $('#troca-historia').replaceWith('<p id="troca-historia">A cada 6 meses novas pessoas entram na DJR. Muita gente já passou por aqui!</p>');
 	});
-
 
 	//SEGUNDO CARROSEL
 	//detecta o antigo item para retirar o .gif e troca por .png
@@ -28,12 +27,11 @@ $(document).ready(function(){
 	});
 	//detecta o novo item para colocar o .gif
 	owl2.on('translated.owl.carousel', function(event) {
-		let altNova = $('.center > img.img-func').attr("alt");
+		let altNova = $('.center > img.img-func').attr("alt"); //seleciona imagem pelo atributo alt
 		$('.center > img.img-func').attr('src', './imgs/funcionamento/'+altNova+'-animado.gif');
 		if (altNova == "01") $('#troca-func').replaceWith('<p id="troca-func"><strong>IDENTIDADE VISUAL</strong><br>Desenvolvemos logos, banners, folders, uniformes, cartazes, cartões de visita e todos os materiais da identidade visual que um projeto ou empresa precisar.</p>');
 		if (altNova == "02") $('#troca-func').replaceWith('<p id="troca-func"><strong>BRINDES</strong><br>Trabalhamos com tipos de brindes mais variados, como chaveiros, canecas, adesivos e outros.</p>');
 		if (altNova == "03") $('#troca-func').replaceWith('<p id="troca-func"><strong>WEB</strong><br>Fazemos layouts de sites, identidade para redes sociais, assinaturas digitais e outros materiais envolvendo divulgação digital.</p>');
 		if (altNova == "04") $('#troca-func').replaceWith('<p id="troca-func"><strong>OUTROS</strong><br>Atendemos a pedidos de materiais específicos de acordo com a viabilidade deles.</p>');
 	});
-
 });
